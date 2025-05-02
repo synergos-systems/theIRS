e8536f48fcdd8f6e866613d091a5c05f096a4522
DeliveranceTechSolutions <kirklincoln@gmail.com>
2025-05-02 05:51:28 -0400

    all templates are generating for every schedule so I can implement the front end, neeed to add a new type 'QName'

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strings"
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strings"
 )
 
 func confirmation(s string, tries int) bool {
     r := bufio.NewReader(os.Stdin)
 
-	for ; tries > 0; tries-- {
-		fmt.Printf("%s Proceed? [y/n]: ", s)
+    for ; tries > 0; tries-- {
+        fmt.Printf("%s Proceed? [y/n]: ", s)
 
-		res, err := r.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-		// Empty input (i.e. "\n")
-		if len(res) < 2 {
-			continue
-		}
+        res, err := r.ReadString('\n')
+        if err != nil {
+            log.Fatal(err)
+        }
+        // Empty input (i.e. "\n")
+        if len(res) < 2 {
+            continue
+        }
 
-		return strings.ToLower(strings.TrimSpace(res))[0] == 'y'
-	}
+        return strings.ToLower(strings.TrimSpace(res))[0] == 'y'
+    }
 
     return false
 }
@@ -37,41 +37,48 @@ func main() {
         fmt.Println("too many")
         return 
     }
-    
+
 
 
     switch os.Args[1] {
-        case "zips":
-            proceed := confirmation(`
-                This will delete any and all zip files in the ./data directory.
-                Do not proceed with this command if you have already used it.
+    case "zips":
+        proceed := confirmation(`
+        This will delete any and all zip files in the ./data directory.
+        Do not proceed with this command if you have already used it.
 
-            `, 3)
-            if proceed {
-                zips, err := UnpackZips()
-                if err != nil {
-                    fmt.Println(err)
-                }
-                fmt.Println(zips)
-            } else {
-                fmt.Println("Aborting")
-            }
-            break
-
-        case "schemas":
-            versions, err := UnpackSchemas()
+        `, 3)
+        if proceed {
+            zips, err := UnpackZips()
             if err != nil {
                 fmt.Println(err)
             }
-            links := generateLinks(versions) 
+            fmt.Println(zips)
+        } else {
+            fmt.Println("Aborting")
+        }
+        break
 
-            for _, uri := range links {
-                fmt.Println(uri)
-            }
-            break
-        
-        default:
-            fmt.Println("the argument provided doesn't exist")
+    case "schemas":
+       // versions, err := UnpackSchemas()
+       // if err != nil {
+       //     fmt.Println(err)
+       // }
+       // links := generateLinks(versions) 
+       // fmt.Println(links)
+       // UnzipSchemas()
+       files, err := GlobWalk("./data/990_xsd/output", "*.xsd")
+       if err != nil {
+        fmt.Println(err)
+       }
+        fmt.Println(files) 
+       for _, file := range files {
+        SchemaGenerator(file)
+       }
+//        SchemaGenerator(uri)
+        break
+
+    default:
+        fmt.Println("the argument provided doesn't exist")
     }
 }
 
--- schemas.go
+++ schemas.go
@@ -1,18 +1,108 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gocomply/xsd2go/pkg/xsd2go"
 )
 
-
-func SchemaGenerator(uri string) {
+func UnzipSchemas() {
     entries, err := os.ReadDir("./data/990_xsd/")
     if err != nil {
         fmt.Println(err)
     }
 
     for _, val := range entries {
+        dst := "./data/990_xsd/output"
         fmt.Println(val)
+        archive, err := zip.OpenReader("./data/990_xsd/" + val.Name())
+        if err != nil {
+            panic(err)
+        }
+        defer archive.Close()
+
+        for _, f := range archive.File {
+            filePath := filepath.Join(dst, f.Name)
+            fmt.Println("unzipping file ", filePath)
+
+            if !strings.HasPrefix(filePath, filepath.Clean(dst)+string(os.PathSeparator)) {
+                fmt.Println("invalid file path")
+                return
+            }
+            if f.FileInfo().IsDir() {
+                fmt.Println("creating directory...")
+                os.MkdirAll(filePath, os.ModePerm)
+                continue
+            }
+
+            if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+                panic(err)
+            }
+
+            dstFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+            if err != nil {
+                panic(err)
+            }
+
+            fileInArchive, err := f.Open()
+            if err != nil {
+                panic(err)
+            }
+
+            if _, err := io.Copy(dstFile, fileInArchive); err != nil {
+                panic(err)
+            }
+            
+            dstFile.Close()
+            fileInArchive.Close()
+        }
+    }
+}
+
+func GlobWalk(rootDir, pattern string) ([]string, error) {
+    var matches []string
+
+    err := filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, walkErr error) error {
+        if walkErr != nil {
+            // abort the walk on error
+            return walkErr
+        }
+
+        if d.IsDir() {
+            // skip directories
+            return nil
+        }
+
+        ok, err := filepath.Match(pattern, filepath.Base(path))
+        if err != nil {
+            // bad pattern
+            return err
+        }
+        if ok {
+            matches = append(matches, path)
+        }
+        return nil
+    })
+    if err != nil {
+        return nil, err
+    }
+    return matches, nil
+}
+
+func SchemaGenerator(uri string) {
+    err := xsd2go.Convert(
+        uri,
+        "main",
+        "./data/990_xsd/output/generated_templates",
+        nil,
+    )
+    if err != nil {
+        fmt.Println(err)
     }
 }
