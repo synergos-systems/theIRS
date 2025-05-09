493371c58fc5e13bef70a04ee1e6519fb34aacd0
DeliveranceTechSolutions <kirklincoln@gmail.com>
2025-05-08 22:47:32 -0400

    unzipping xmls, needs testing to ensure all are unzipped, idk how that would happen except for 2019 <= 2019 <= 2020 <= 2020 then handcheck the last installment everyday but that's kind of insane

--- parser.go
+++ parser.go
@@ -5,51 +5,82 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-func XMLParser() {
-    unzipFile()
-}
-
-func unzipFile() {
+func UnzipXMLs() {
     pathway := "./data/990_zips/"
+
     reader, err := os.ReadDir(pathway)
     if err != nil {
         fmt.Println(err)
     }
 
-
     for _, zipper := range reader {
-        zipperName := pathway + zipper.Name() 
-
-
-        zReader, err := zip.OpenReader(zipperName)
-        if err != nil {
-            fmt.Println(err)
-        }
-        fmt.Println(zipperName[:len(zipperName) - 4])
-        err = os.MkdirAll(zipperName[:len(zipperName) - 4], os.FileMode(os.O_CREATE | os.O_TRUNC | os.O_RDWR))
-        if err != nil {
-            fmt.Println(err)
-        }
-
-        for _, file := range zReader.File {
-            rc, err := file.Open()
-            if err != nil {
-                fmt.Println(err)
-            }
-
-            newFile, err := os.Open(zipperName[:len(zipperName) - 4])
-            if err != nil {
-                fmt.Println(err)
-            }
-
-            io.Copy(newFile, rc)
-        }
-
-        zReader.Close()
+        template := pathway + zipper.Name()
+        unzipXMLs(template, template[:len(template) - 4])
     }
-    
+
 }
 
+func unzipXMLs(src, dest string) error {
+    r, err := zip.OpenReader(src)
+    if err != nil {
+        return err
+    }
+    defer func() {
+        if err := r.Close(); err != nil {
+            panic(err)
+        }
+    }()
+
+    os.MkdirAll(dest, 0777)
+    extractAndWriteFile := func(f *zip.File) error {
+        rc, err := f.Open()
+        if err != nil {
+            return err
+        }
+        defer func() {
+            if err := rc.Close(); err != nil {
+                panic(err)
+            }
+        }()
+
+        path := filepath.Join(dest, f.Name)
+        if !strings.HasPrefix(path, filepath.Clean(dest) + string(os.PathSeparator)) {
+            return fmt.Errorf("illegal file path: %s", path)
+        }
+
+        if f.FileInfo().IsDir() {
+            os.MkdirAll(path, f.Mode())
+        } else {
+            os.MkdirAll(filepath.Dir(path), f.Mode())
+            f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+            if err != nil {
+                return err
+            }
+            defer func() {
+                if err := f.Close(); err != nil {
+                    panic(err)
+                }
+            }()
+
+            _, err = io.Copy(f, rc)
+            if err != nil {
+                return err
+            }
+        }
+        return nil
+    }
+
+    for _, f := range r.File {
+        err := extractAndWriteFile(f)
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
 
